fd8ce29f067e69af4288ea73efbd503b317e690c
Ritwika Ghosh <ritwikaghosh48@gmail.com>
2020-09-22 08:21:29 +0530

    Added CORS settings

--- app/__init__.py
+++ app/__init__.py
@@ -2,14 +2,13 @@ from flask import Flask
 from app.configuration import db, config
 from flask_mongoengine import MongoEngine
 from app.models.Moves import Moves
+from flask_cors import CORS
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object(config.DevConfig())
+CORS(app, resources={r"/*": {"origins": "*"}})
 db.initialize_db(app)
 
 @app.route("/")
 def index():
-    Moves(
-        token = "1232434"
-    ).save()
     return "Welcome to Count4 game!"
