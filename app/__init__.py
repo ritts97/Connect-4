39805dbd40a95e445501506225c0524afebc05af
Ritwika Ghosh <ritwikaghosh48@gmail.com>
2020-09-22 08:12:03 +0530

    Added mongoengine support

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from app.configuration import db, config
 
-app = Flask(__name__)
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_object(config.DevConfig())
+db.initialize_db(app)
 
 @app.route("/")
 def index():
---
+++ app/configuration/config.py
@@ -0,0 +1,8 @@
+class DevConfig:
+    DEBUG = True
+    DEVELOPMENT = True
+    MONGODB_DB = 'game'
+    MONGODB_HOST = 'mongodb+srv://Ritwika:abcd1234@cluster0.bmv2i.gcp.mongodb.net/game?retryWrites=true&w=majority'
+    PROPAGATE_EXCEPTIONS = True
+    SECRET_KEY = 'pratilipi'
+    FLASK_SECRET = SECRET_KEY
