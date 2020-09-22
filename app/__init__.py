6109104a70ad46b280a3e89d254ba6caef96ac04
Ritwika Ghosh <ritwikaghosh48@gmail.com>
2020-09-22 08:38:41 +0530

    Implemented token with session and created start API

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
-from flask import Flask
+from flask import Flask, request, url_for, Response, jsonify, session
 from app.configuration import db, config
 from flask_mongoengine import MongoEngine
 from app.models.Moves import Moves
 from flask_cors import CORS
+from uuid import uuid4
+import numpy as np
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object(config.DevConfig())
@@ -12,3 +14,18 @@ db.initialize_db(app)
 @app.route("/")
 def index():
     return "Welcome to Count4 game!"
+
+@app.route('/start')
+def start():
+    try:
+        body = request.data
+        if body.decode('utf-8') == 'START':
+            token = str(uuid4())
+            session['token'] = token
+            resp = {
+                "token" : token,
+                "message" : "READY"
+            }
+            return jsonify(resp)
+    except Exception as e:
+        return e
