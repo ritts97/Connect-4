34c726937f457b96233ba265ca44f2e618935f20
Ritwika Ghosh <ritwikaghosh48@gmail.com>
2020-09-22 08:51:43 +0530

    Modified mongoengine model

--- app/models/Moves.py
+++ app/models/Moves.py
@@ -4,5 +4,7 @@ from app.configuration.db import db
 class Moves(db.Document):
     token = db.StringField(required=True, unique=True)
     matrix = db.BinaryField()
+    turn = db.IntField(default = 1)
+    gameValid = db.BooleanField(default = True)
     moves = db.ListField()
     winner = db.StringField()
